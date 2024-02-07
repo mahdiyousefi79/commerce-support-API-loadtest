a68ac0e3f6ac812a11470578d8ac6d73bce65590
myouse652 <mahdi_yousefibaviloliaei@comcast.com>
2024-02-06 17:50:01 -0800

    update

--- src/test/scala/com/comcast/commerce/CommerceSupportAPISimulation.scala
+++ src/test/scala/com/comcast/commerce/CommerceSupportAPISimulation.scala
@@ -25,7 +25,7 @@ trait CommerceBase {
     Instant.ofEpochMilli(millis)
   }
     val supportAPI = http("supportAPI")
-      .get(s"${config("commerceService.url")}/commerce/subscriptions/support/sku/59b8293e-e699-434d-ae0a-2e314cd10589?timeZoneMinutes=-300")
+      .post(s"${config("commerceService.url")}/commerce/subscriptions/support/sku/59b8293e-e699-434d-ae0a-2e314cd10589?timeZoneMinutes=-300")
       .header("Authorization", "#{bearer}")
       .header("oat", "#{oat}")
       .header("Content-Type", "application/json")
